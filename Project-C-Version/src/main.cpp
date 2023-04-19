7b31259624f682ab05ea1d4d6271d8e2b1f22d6f
3of26 <76955501+appleshaman@users.noreply.github.com>
2023-04-19 19:30:12 +1200

    it now can run a simple pattern of life of game

--- Project-C-Version/src/main.cpp
+++ Project-C-Version/src/main.cpp
@@ -1,34 +1,50 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 
-#define X_FIRST_BLOCK 75
-#define Y_FIRST_BLOCK 30
-
-struct squareBlock {
-  int x_pos = 0;
-  int y_pos = 0;
-  bool alive = false;
-};
-squareBlock blocks[8][8];
+#define X_FIRST_BLOCK 140
+#define Y_FIRST_BLOCK 10
 
 void setupScreen();
 void initBlocks();
 void printBlocks();
+void setBlockStatus();
+void findNeighbour();
+void setBlinker();
+void debugPrintNeighbour();
+void printGridInfo();
 
+struct squareBlock {
+  int x_pos = 0;
+  int y_pos = 0;
+  int neighbour_num = 0;
+  bool alive = false;
+};
+squareBlock blocks[8][8];
 TFT_eSPI tft = TFT_eSPI();
+
+int generation = 0;
+int cellNum = 0;
+bool ifRunning = false;
 
 void setup() {
   Serial.begin(921600);
   Serial.println("Start!");
   setupScreen();
   initBlocks();
-
+  setBlinker();
+  printBlocks();
+  delay(1000);
 }
 
 void loop() {
   tft.fillScreen(tft.color565(56, 178, 92)); // 0x38b25c
+  findNeighbour();
+  setBlockStatus();
   printBlocks();
-  delay(3000);
+  debugPrintNeighbour();
+  generation ++;
+  printGridInfo();
+  delay(1000);
   
 }
 
@@ -50,6 +66,7 @@ void initBlocks(){//set correct initial position for each blocks
 }
 
 void printBlocks(){
+  tft.drawRect(X_FIRST_BLOCK - 3, Y_FIRST_BLOCK - 3 , 92, 92, TFT_WHITE);
   for(int i = 0; i < 8; i ++){
     for(int j = 0; j < 8; j ++){
       if(blocks[i][j].alive){
@@ -59,4 +76,109 @@ void printBlocks(){
       }
     }
   }
+}
+
+void setBlockStatus(){
+  for(int i = 0; i < 8; i ++){
+    for(int j = 0; j < 8; j ++){
+      if(blocks[i][j].alive){
+        if(blocks[i][j].neighbour_num < 2){
+          blocks[i][j].alive = false;
+          cellNum --;
+        }
+        else if(blocks[i][j].neighbour_num > 3){
+          blocks[i][j].alive = false;
+          cellNum --;
+        }
+      }else if(!blocks[i][j].alive){
+        if(blocks[i][j].neighbour_num == 3){
+          blocks[i][j].alive = true;
+          cellNum ++;
+        }
+      }
+    }
+  }
+}
+
+void findNeighbour(){
+  for(int i = 0; i < 8; i ++){
+    for(int j = 0; j < 8; j ++){
+      blocks[i][j].neighbour_num = 0;// clear the number of neighbour
+      int neighbourNum = 0;
+
+      if(((i - 1) > 0)&&((j - 1) > 0)){
+        if(blocks[i - 1][j - 1].alive){
+          neighbourNum ++;
+        }
+      }
+      if((i - 1) > 0){
+        if(blocks[i - 1][j].alive){
+          neighbourNum ++;
+        }
+      }
+      if(((i - 1) > 0)&&((j + 1) < 8)){
+        if(blocks[i - 1][j + 1].alive){
+          neighbourNum ++;
+        }
+      }
+      if((j - 1) > 0){
+        if(blocks[i][j - 1].alive){
+          neighbourNum ++;
+        }
+      }
+      if((j + 1) < 8){
+        if(blocks[i][j + 1].alive){
+          neighbourNum ++;
+        }
+      }
+      if(((i + 1) < 8)&&((j - 1) > 0)){
+        if(blocks[i + 1][j - 1].alive){
+          neighbourNum ++;
+        }
+      }
+      if((i + 1) < 8){
+        if(blocks[i + 1][j].alive){
+          neighbourNum ++;
+        }
+      }
+      if(((i + 1) < 8)&&((j + 1) < 8)){
+        if(blocks[i + 1][j + 1].alive){
+          neighbourNum ++;
+        }
+      }
+
+      blocks[i][j].neighbour_num = neighbourNum;
+
+    }
+  }
+}
+
+void debugPrintNeighbour(){
+  for(int i = 0; i < 8; i ++){
+    for(int j = 0; j < 8; j ++){
+      Serial.print(" ");
+      Serial.print(blocks[i][j].neighbour_num);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+  Serial.println();
+}
+
+void printGridInfo(){
+  tft.setTextFont(2);
+  tft.setTextColor(TFT_WHITE);
+  tft.drawString("Genation:", 140, 100);
+  tft.drawString("Cell Number:", 140, 115);
+  tft.setCursor(201, 100);
+  tft.print(generation);
+  tft.setCursor(221, 115);
+  tft.print(cellNum);
+  
+}
+
+void setBlinker(){
+  blocks[3][1].alive = true;
+  blocks[3][2].alive = true;
+  blocks[3][3].alive = true;
 }
